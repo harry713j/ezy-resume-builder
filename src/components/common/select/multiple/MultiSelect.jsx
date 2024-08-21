f472b8e573fa46f9c4f0aafdfea3c890d6e4910f
Harry <hariharasethi89@gmail.com>
2024-08-21 19:29:15 +0530

    Make Select, MultiSelect & DatePicker components

--- src/components/common/select/multiple/MultiSelect.jsx
+++ src/components/common/select/multiple/MultiSelect.jsx
@@ -1,8 +1,63 @@
-import React from "react";
+import React, { useState } from "react";
+import {
+  FormControl,
+  InputLabel,
+  MenuItem,
+  OutlinedInput,
+  Select,
+  Box,
+  Chip,
+} from "@mui/material";
+import { Clear } from "@mui/icons-material";
 import "./multiSelect.scss";
 
-function MultiSelect() {
-  return <div>MultiSelect</div>;
-}
-
+const MultiSelect = React.forwardRef(function (
+  { label, items, value, onChange, ...props },
+  ref
+) {
+  const handleDelete = (chipToDelete) => {
+    onChange({
+      target: {
+        value: value.filter((v) => v !== chipToDelete),
+      },
+    });
+  };
+  return (
+    <FormControl className="multiselect__container">
+      <InputLabel className="multiselect__label" id="multi-select-chip-label">
+        {label}
+      </InputLabel>
+      <Select
+        ref={ref}
+        label={label}
+        multiple
+        value={value || []}
+        onChange={onChange}
+        labelId="multi-select-chip-label"
+        className="multi__select"
+        input={<OutlinedInput id="multi-select-chip" label={label} />}
+        renderValue={(selected) => (
+          <Box sx={{ display: "flex", flexWrap: "wrap", gap: 0.5 }}>
+            {selected.map((value) => (
+              <Chip
+                sx={{ backgroundColor: "#E4F6FF" }}
+                key={value}
+                label={value}
+                onDelete={handleDelete(value)}
+                deleteIcon={<Clear />}
+              />
+            ))}
+          </Box>
+        )}
+        {...props}
+      >
+        {items.map((item) => (
+          <MenuItem key={item} value={item} className="multiselect__item">
+            {item}
+          </MenuItem>
+        ))}
+      </Select>
+    </FormControl>
+  );
+});
 export default MultiSelect;
--- src/components/common/select/single/SingleSelect.jsx
+++ src/components/common/select/single/SingleSelect.jsx
@@ -1,8 +1,32 @@
 import React from "react";
+import { FormControl, MenuItem, Select, InputLabel } from "@mui/material";
+
 import "./singleSelect.scss";
 
-function SingleSelect() {
-  return <div>SingleSelect</div>;
-}
+const SingleSelect = React.forwardRef(function (
+  { label, items, ...props },
+  ref
+) {
+  return (
+    <FormControl className="select__container">
+      <InputLabel className="select__label" id="select-label">
+        {label}
+      </InputLabel>
+      <Select
+        ref={ref}
+        label={label}
+        labelId="select-label"
+        className="select"
+        {...props}
+      >
+        {items.map((item) => (
+          <MenuItem key={item} value={item} className="select__item">
+            {item}
+          </MenuItem>
+        ))}
+      </Select>
+    </FormControl>
+  );
+});
 
 export default SingleSelect;
